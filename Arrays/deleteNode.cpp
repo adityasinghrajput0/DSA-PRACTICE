bbaefbf3c8a35b6396d2a682c1700ce105825702
adityasinghrajput0 <adityasewak74@gmail.com>
2025-11-16 19:34:58 +0530

    node deletion in single LL

---
+++ Arrays/deleteNode.cpp
@@ -0,0 +1,32 @@
+class Node {
+    private:
+        int data;
+        Node* next;
+    public:
+        Node (int data) {
+            this->data=data;
+            this->next=NULL;
+        }
+}
+
+Node* removeK(int k,Node* head) {
+    if (head==NULL) return NULL;
+    if (k==1) {
+        Node* temp=head;
+        head=head->next;
+        delete temp;
+        return head;
+    }
+    Node* temp=head,prev=NULL;
+    int cnt=0;
+    while (temp != NULL) {
+        cnt++;
+        if (cnt==k) {
+            prev->next=prev->next->next;
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+    return head;
+}
